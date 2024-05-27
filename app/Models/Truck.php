56f4f241bd2c4a47b888fb308c724840c3a9da14
AlvaroPerez018 <alvaroperez0172022@gmail.com>
2024-05-26 23:36:00 -0500

    refuerzo examen laravel

---
+++ app/Models/Truck.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Truck extends Model
+{
+    use HasFactory;
+
+    public function Truck_driver(){
+        return $this->belongsToMany('App\Models\Truck_driver');
+    }
+
+}
